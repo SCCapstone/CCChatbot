84c207765d6c4df78682d464e6b24428aca42e32
StevenMcGown <46934571+StevenMcGown@users.noreply.github.com>
2019-11-29 13:28:44 -0500

    Add files via upload
    Files edited from the original angular upload

---
+++ Github/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ChatModule } from './chat/chat.module'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initiliazeApp(firebase),
+    AngularFireAuthModule,
+    ChatModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
