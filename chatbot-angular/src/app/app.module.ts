d5b826a7ad43a3a1c6456aa9d2a7668bd5b50dec
StevenMcGown <46934571+StevenMcGown@users.noreply.github.com>
2019-12-06 20:43:35 -0500

    Update app.module.ts

--- chatbot-angular/src/app/app.module.ts
+++ chatbot-angular/src/app/app.module.ts
@@ -4,9 +4,24 @@ import { ChatModule } from './chat/chat.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { HomePageComponent } from './home-page/home-page.component';
 import { AgentPageComponent } from './agent-page/agent-page.component';
 
+
+const firebase = {
+  apiKey: 'AIzaSyD7d2VTNye12itFkj7rEIz3Mlw7wupdmXA',
+  authDomain: 'acmedemo-tefspy.firebaseapp.com',
+  dataBaseURL: 'https://acmedemo-tesfpy.firebaseio.com',
+  projectId: 'acmedemo-tefspy',
+  storageBucket: 'acmedemo-tefspy.appspot.com',
+  messagingSenderId: '800812168325',
+  appId: '1:800812168325:web:dbf8b9cd42f03d0c50e5ec'
+};
+
+//import { firebase } from '../env';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +31,9 @@ import { AgentPageComponent } from './agent-page/agent-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChatModule
+    ChatModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
