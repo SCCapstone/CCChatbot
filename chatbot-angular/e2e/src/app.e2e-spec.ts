4eddfb6cda9e1a7b7a92e4881f72807a4f4a3289
jregoUSC <jrego@email.sc.edu>
2020-02-02 02:15:42 -0500

    Added Testing cases to App

--- chatbot-angular/e2e/src/app.e2e-spec.ts
+++ chatbot-angular/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-app app is running!');
+    expect(page.getTitleText()).toEqual('Home Page Title');
   });
 
   afterEach(async () => {
