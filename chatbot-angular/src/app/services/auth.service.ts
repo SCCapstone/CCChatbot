fb437fd557de23b3415453baea1e7574c105d573
Jahred Danker <jahrede@gmail.com>
2020-02-20 12:37:21 -0500

    created backend service for authentication #102

---
+++ chatbot-angular/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+}) 
+export class AuthService {
+  user$: Observable<User>;  // observable to custom interface
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) { 
+    this.user$ = this.afAuth.authState
+
+    
+  }
+}
