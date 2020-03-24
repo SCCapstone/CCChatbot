282fdf610bbf9d6d910a78076ed1d736adacd991
Jahred Danker <jahrede@gmail.com>
2020-03-24 19:36:17 -0400

    deployed most recent changes

--- chatbot-angular/src/app/app.component.ts
+++ chatbot-angular/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth'
 import { LoginService } from './auth/login.service';
-import { WebSocketService } from './services/web-socket.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
