3a840c059afd66902e2cabece68aaf27a15ae49c
Jahred Danker <jahrede@gmail.com>
2020-04-02 18:53:54 -0400

    finsihed download function #116
    needs button to trigger function fixed

--- chatbot-angular/src/app/download/download.component.ts
+++ chatbot-angular/src/app/download/download.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
 import { Observable } from 'rxjs/observable';
 import * as firebase from 'firebase';
 
@@ -10,13 +9,42 @@ import * as firebase from 'firebase';
 })
 export class DownloadComponent implements OnInit {
 
-  constructor(private afStorage: AngularFireStorage) { }
-
-  ngOnInit() {  }
-
+  constructor() { }
   download(){
-    var storage = firebase.storage();
-    var pathReference = storage.ref('assets/download.html');
+    var gsReference = firebase.storage().refFromURL('gs://acmedemo-tefspy.appspot.com/assets/download.html')
+
+    gsReference.getDownloadURL().then(function(url){
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = function(event) {
+        var blob = xhr.response;
+      };
+      xhr.open('GET', url);
+      xhr.send();
+    }).catch(function(error){
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+    
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+      
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
+
   }
+
+  ngOnInit() {
+    var btn = document.getElementById('download-btn')
+
+
+    }
+
+
 
 }
